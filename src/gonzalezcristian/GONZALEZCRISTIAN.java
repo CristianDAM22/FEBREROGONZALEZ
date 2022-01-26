43892853a3ab8b8120328e620fe665a24c652ab2
Cristian <>
2022-01-26 16:25:04 +0100

    PRIMER COMMIT

---
+++ src/gonzalezcristian/GONZALEZCRISTIAN.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gonzalezcristian;
+
+/**
+ *
+ * @author dam
+ */
+public class GONZALEZCRISTIAN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("LINEA UNO");
+        System.out.println("LINEA DOS");
+    }
+    
+}
